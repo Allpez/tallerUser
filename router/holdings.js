87fa2368c15d335bca4ec07d96aa122d9851f50b
Allpez <alexlop1987@gmail.com>
2024-11-13 20:29:39 -0500

    middleware passport

--- router/holdings.js
+++ router/holdings.js
@@ -5,15 +5,16 @@ import update from "../controllers/holdings/update.js";
 import deleteOne from "../controllers/holdings/delete.js";
 import validator from "../middlewares/validator.js";
 import schemaHoldingsCreate from "../schemas/holdings/create.js";
+import passport from "../middlewares/passport.js";
 
 const routerHoldings = Router()
 
-routerHoldings.get('/all', allHolding)
-routerHoldings.get('/id/:id', holdingByID)
-routerHoldings.get('/user/:userId', holdingByUser)
-routerHoldings.post('/create', validator(schemaHoldingsCreate), create)
-routerHoldings.post('/insertmany', insertMany)
-routerHoldings.put('/update', validator(schemaHoldingsCreate), update)
-routerHoldings.delete('/deleteOne', deleteOne)
+routerHoldings.get('/all', passport.authenticate('jwt', { session: false }), allHolding)
+routerHoldings.get('/id/:id', passport.authenticate('jwt', { session: false }), holdingByID)
+routerHoldings.get('/user/:userId', passport.authenticate('jwt', { session: false }), holdingByUser)
+routerHoldings.post('/create', passport.authenticate('jwt', { session: false }), validator(schemaHoldingsCreate), create)
+routerHoldings.post('/insertmany', passport.authenticate('jwt', { session: false }), validator(schemaHoldingsCreate), insertMany)
+routerHoldings.put('/update', passport.authenticate('jwt', { session: false }), validator(schemaHoldingsCreate), update)
+routerHoldings.delete('/deleteOne', passport.authenticate('jwt', { session: false }), deleteOne)
 
 export default routerHoldings
